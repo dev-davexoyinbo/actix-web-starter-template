c8f2efa8c9e1afc20eb1a8bfa72b17293744d9ae
David Oyinbo <dev.davexoyinbo@gmail.com>
2025-04-24 19:53:30 +0100

    fix: migrations not working

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -12,6 +12,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
+            Box::new(m20250324_002438_create_users_table::Migration),
             Box::new(m20250329_214625_create_roles_table::Migration),
             Box::new(m20250329_214907_create_permissions_table::Migration),
             Box::new(m20250329_215624_create_role_permissions_table::Migration),
